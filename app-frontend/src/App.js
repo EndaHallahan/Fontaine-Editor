d74541bd0b92e0bf642e7feb4864a3744b800ec8
EndaHallahan <endah@udel.edu>
2020-05-27 15:19:20 -0500

    Moved frontend-specific items to 'App' component in preparation for backend interfacing.

--- app-frontend/src/App.js
+++ app-frontend/src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import ModalHandler from "./components/ModalHandler";
 import Workspace from "./components/Workspace";
 import SettingsHandler from "./components/SettingsHandler";
 import ShortcutHandler from "./components/ShortcutHandler";
 import DistractionFree from "./components/DistractionFree";
 
+import "./styles/index.scss";
+
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<SettingsHandler />
-				<ModalHandler />
-				<ShortcutHandler />
-				<DistractionFree />
-				<main>
-					<Workspace />
-				</main>
-				<footer>
-				</footer>
-			</div>
+			<Provider store={store}>
+				<div className="App">
+					<SettingsHandler />
+					<ModalHandler />
+					<ShortcutHandler />
+					<DistractionFree />
+					<main>
+						<Workspace />
+					</main>
+					<footer>
+					</footer>
+				</div>
+			</Provider>
 		);
 	}
 }
--- app-frontend/src/index.js
+++ app-frontend/src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import store from './store';
-import { Provider } from 'react-redux';
+
 import * as serviceWorker from './serviceWorker';
 
-import "./styles/index.scss";
-
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-  		<App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  	<React.StrictMode>
+		<App />
+  	</React.StrictMode>,
+  	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
